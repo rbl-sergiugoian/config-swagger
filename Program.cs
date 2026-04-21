a34cd790fc01adeb97cdbceea8b180e0662f8a0b
rbl-sergiugoian <sergiu.goian@rebeldot.com>
2026-04-21 16:09:51 +0300

    commit 4

--- Program.cs
+++ Program.cs
@@ -7,11 +7,13 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
 app.UseApiDocumentation();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
