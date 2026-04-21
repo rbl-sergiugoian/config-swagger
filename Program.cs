e74e42521d1a2323b28fba58cd56188ca8508b15
rbl-sergiugoian <sergiu.goian@rebeldot.com>
2026-04-21 16:04:59 +0300

    delete comments

--- Program.cs
+++ Program.cs
@@ -2,18 +2,13 @@ using swager.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddApiDocumentation();
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.UseApiDocumentation();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
