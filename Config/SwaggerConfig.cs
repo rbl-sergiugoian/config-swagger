c06c28abad51bd0106989860d85f920eb318fb4c
rbl-sergiugoian <sergiu.goian@rebeldot.com>
2026-04-21 15:51:54 +0300

    initial commit

---
+++ Config/SwaggerConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.OpenApi;
+
+namespace swager.Config;
+
+public static class SwaggerConfig
+{
+    public static IServiceCollection AddApiDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "test api",
+                Version = "v1"
+            });
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseApiDocumentation(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+using swager.Config;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApiDocumentation();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.UseApiDocumentation();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
