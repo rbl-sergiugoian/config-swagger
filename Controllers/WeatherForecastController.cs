bff183010182b980cd207e911b3950e8d6c7d910
rbl-sergiugoian <sergiu.goian@rebeldot.com>
2026-04-21 16:17:41 +0300

    c2

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace swager.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
