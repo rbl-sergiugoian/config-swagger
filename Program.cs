01d8d94ad46c2e6907644a20b1060501361d2283
rbl-sergiugoian <sergiu.goian@rebeldot.com>
2026-04-21 16:16:53 +0300

    c2 (+1 squashed commits)
    Squashed commits:
    
    [3aecaed] c1
    
    redundant changes

--- Program.cs
+++ Program.cs
@@ -9,13 +9,16 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 app.UseApiDocumentation();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
